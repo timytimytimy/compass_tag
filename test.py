de0286fcc02fef8fa2347242b674b67c103b6159
timytimytimy <530967072@qq.com>
2016-08-05 22:04:48 +0800

    modify test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.spatial.distance
 from itertools import combinations, chain
+import re
 allsubsets = lambda n: list(chain(*[combinations(n, ni) for ni in range(len(n) + 1)]))
 
 
@@ -27,8 +28,10 @@ def read_model():
         ret[subset] = np.array(v)
     return ret
 
-def test():
-    packages = set([line.strip() for line in open('test')])
+def predict(project, packages):
+    # packages = set([line.strip() for line in open('test')])
+    if len(packages) == 0:
+        return
     model = read_model()
     vector = np.array([1 if package in packages else 0 for package in model['_packages']])
     max_tag = ''
@@ -38,7 +41,23 @@ def test():
             r = scipy.spatial.distance.cosine(model[tags], vector)
             if r > max_r:
                  max_r, max_tag = r, tags
-    print max_r, max_tag
+    print '%s,%s,%s' % (project, max_r, ','.join(max_tag))
+
+def read_predict():
+    pattern = re.compile("^([0-9a-zA-Z]+\.)+[0-9a-zA-Z]+$")
+    f = open('test')
+    last = ''
+    packages = set()
+    for line in f.readlines():
+        items = line.strip().split(',')
+        name = items[0].strip()
+        package = items[1].strip()
+        if last == name and pattern.match(package):
+            packages.add(package)
+        elif last != name:
+            predict(last, packages)
+            last = name
+            packages = set()
 
 if __name__ == '__main__':
-    test()
+    read_predict()
